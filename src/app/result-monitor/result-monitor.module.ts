3b19de3296b3ea63f34d905f5c389eb9b8513b4d
johnisawesome94 <lunde367@umn.edu>
2023-08-08 14:08:33 -0500

    2 files modified

--- src/app/result-monitor/result-monitor.module.ts
+++ src/app/result-monitor/result-monitor.module.ts
@@ -1,3 +1,4 @@
+import { NgFor } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -7,7 +8,7 @@ import { OverallResultsComponent } from './overall-results/overall-results.compo
 import { ResultMonitorComponent } from './result-monitor.component';
 
 @NgModule({
-  imports: [MatButtonModule, MatCardModule, MatGridListModule],
+  imports: [MatButtonModule, MatCardModule, MatGridListModule, NgFor],
   declarations: [
     IndividualResultsComponent,
     OverallResultsComponent,
